b99aa6860ff3b643c939880d93f0335f45fe76ab
KrayzeX <1alexolex1@gmail.com>
2018-11-08 12:45:46 +0300

    First Function

---
+++ FirstFunc.js
@@ -0,0 +1,12 @@
+
+const quadraticEquation = (a,b,c)=>{
+    var d,x1,x2;
+    d=(Math.pow(b,2))-4*a*c;
+    if (d >= 0){
+        x1=(-b+Math.sqrt(d))/2*a;
+        x2=(-b-Math.sqrt(d))/2*a;
+    }else if (d < 0){
+        return "Error! I can't solve the equation!";
+    }
+    return x1,x2;
+}
