cc7c0f9b240824424756be63aa89e3b2941f707e
KrayzeX <1alexolex1@gmail.com>
2018-11-08 17:56:51 +0300

    Successful test

--- FirstFunc.test.js
+++ FirstFunc.test.js
@@ -1,10 +1,18 @@
 const quadraticEquation = require('./FirstFunc')
 
+
 test('Adds 1, -4, 4 to equal 2',() =>{
     expect(quadraticEquation(1,-4,4)).toBe(2);
 })
 
 test('Adds 1, 3, -4 to equal -4, 1',() =>{
     expect(quadraticEquation(1,3,-4)).toBe(-4,1)
-});
+})
 
+test('Adds 1, -5, 9 to equal Error', ()=>{
+    expect(quadraticEquation(1,-5,9)).toString("Error! I can't resolve the equation!")
+})
+
+test('Adds 1, -5, 9 to equal Error', ()=>{
+    expect(quadraticEquation(1,-4,4)).toBe(11)
+})
